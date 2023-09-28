2022f4e828eb3f478a2d4aa6a07a4f8cd6f4fa7a
Eden <s30ngb1n@gmail.com>
2023-09-28 22:23:44 +0900

    feat: router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/home';
+import About from './pages/about/about';
+import Detail from './pages/detail/detail';
+import Tags from './pages/tags/tags';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/detail" element={<Detail />} />
+      <Route path="/tags" element={<Tags />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
