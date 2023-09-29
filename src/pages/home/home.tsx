1a565871f80e80de9e9779371ffc42ba4a845b72
Eden <s30ngb1n@gmail.com>
2023-09-29 21:08:20 +0900

    feat: component header

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from '../../component/header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+      <div>Home</div>
+    </div>
   )
 }
 
