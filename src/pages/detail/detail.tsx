4708e6bc1d8c19e34a09764daaa95576a3796236
Eden <s30ngb1n@gmail.com>
2023-09-30 22:18:59 +0900

    feat: header component 추가

--- src/pages/detail/detail.tsx
+++ src/pages/detail/detail.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../../component/header'
 
 const Detail = () => {
   return (
-    <div>Detail
-
+    <div>
+      <Header />
+      <div>Detail</div>
     </div>
   )
 }
