1d63a61474f5ea243e149a49b6072af126b358ca
Eden <s30ngb1n@gmail.com>
2023-10-03 22:29:56 +0900

    feat: header

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	return (
+		<div>
+			<text>개발 기록</text>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
