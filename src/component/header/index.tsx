ba98845e31bab33d575e66575c4d99e510b5474e
Eden <s30ngb1n@gmail.com>
2023-10-04 23:06:54 +0900

    feat: header style

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<div>
-			<text>개발 기록</text>
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'space-between',
+				borderStyle: 'solid',
+			}}
+		>
+			<div style={{ width: 10, height: 10, backgroundColor: 'black' }} />
+			<div style={{ display: 'flex' }}>
+				<div>개발 기록</div>
+				<div>나에 대해서</div>
+				<div>태그</div>
+			</div>
 		</div>
 	);
 };
