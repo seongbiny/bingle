f63d74da0b8874c5d31e5228bd7243a933662409
Eden <s30ngb1n@gmail.com>
2024-02-21 18:13:13 +0900

    feat: 표지 디자인

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import Header from '../../component/header'
+import React from 'react';
 
 const Home = () => {
-  return (
-    <div>
-      <Header />
-      <div>Home</div>
-    </div>
-  )
-}
+	return (
+		<div
+			style={{
+				backgroundColor: '#6488CF',
+				width: '720pt',
+				height: '405pt',
+			}}
+		>
+			<div style={{ paddingTop: '38pt' }}>
+				<div
+					style={{
+						color: 'white',
+						fontSize: '15pt',
+						fontWeight: 'bold',
+					}}
+				>
+					2024 PORTFOLIO
+				</div>
+				<div
+					style={{
+						color: 'white',
+						fontSize: '90pt',
+						fontWeight: 'bold',
+					}}
+				>
+					윤 성 빈
+				</div>
+				<div style={{ color: '#FCD5B5', fontSize: '21pt' }}>
+					주어진 임무는 어떻게서든 해내는 개발자
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
