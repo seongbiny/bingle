cb97d867fc18ed8cd138d7166213a1b94487aff8
Eden <s30ngb1n@gmail.com>
2024-04-05 12:12:40 +0900

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                'my-blue': '#6488CF',
-                'my-orange': '#FCD5B5',
-            },
-        },
-    },
-    plugins: [],
+export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const theme = {
+	extend: {
+		colors: {
+			'my-blue': '#6488CF',
+			'my-orange': '#FCD5B5',
+		},
+	},
 };
+export const plugins = [];
